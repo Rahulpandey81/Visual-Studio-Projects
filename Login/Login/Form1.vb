dd74eed01b62ceac5ab26e625fef87cb95ea5a4a
Rahul Pandey <rahulpandey.rp1996@gmail.com>
2018-10-29 13:53:53 +0530

    It is a Login Form In visual Studio

---
+++ Login/Login/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+
+    End Sub
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If TextBox1.Text = My.Settings.User And
+            TextBox2.Text = My.Settings.Password Then
+            MsgBox("Welcome")
+            Me.Hide()
+            Form3.Show()
+        Else
+            MsgBox("Wrong Password")
+
+
+        End If
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Form2.Show()
+
+    End Sub
+End Class
---
+++ Login/Login/Form2.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form2
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        My.Settings.User = TextBox1.Text
+        My.Settings.Password = TextBox2.Text
+        My.Settings.Save()
+        MsgBox("Account Created Successfully")
+        Me.Hide()
+        Form1.Show()
+
+
+    End Sub
+End Class
