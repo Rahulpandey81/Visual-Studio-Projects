501d02c70cbfe20b04fc4044871ad237216be73d
Rahul Pandey <rahulpandey.rp1996@gmail.com>
2019-02-24 23:32:49 +0530

    Simple Mail Sending Program in VB.Net

---
+++ Makhanlal_University_Bhopal/Makhanlal_University_Bhopal/Form1.Designer.vb
@@ -0,0 +1,280 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.RichTextBoxMessage = New System.Windows.Forms.RichTextBox()
+        Me.TextBoxTo = New System.Windows.Forms.TextBox()
+        Me.TextBoxSubject = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBoxAttachment = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button1.Location = New System.Drawing.Point(280, 451)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Send"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebBrowser1.Location = New System.Drawing.Point(0, 0)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(686, 497)
+        Me.WebBrowser1.TabIndex = 0
+        '
+        'Button2
+        '
+        Me.Button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button2.Location = New System.Drawing.Point(404, 451)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(111, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Go to Site"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'RichTextBoxMessage
+        '
+        Me.RichTextBoxMessage.Location = New System.Drawing.Point(280, 227)
+        Me.RichTextBoxMessage.Name = "RichTextBoxMessage"
+        Me.RichTextBoxMessage.Size = New System.Drawing.Size(373, 132)
+        Me.RichTextBoxMessage.TabIndex = 3
+        Me.RichTextBoxMessage.Text = ""
+        '
+        'TextBoxTo
+        '
+        Me.TextBoxTo.Location = New System.Drawing.Point(291, 130)
+        Me.TextBoxTo.Name = "TextBoxTo"
+        Me.TextBoxTo.Size = New System.Drawing.Size(362, 20)
+        Me.TextBoxTo.TabIndex = 4
+        '
+        'TextBoxSubject
+        '
+        Me.TextBoxSubject.Location = New System.Drawing.Point(291, 169)
+        Me.TextBoxSubject.Name = "TextBoxSubject"
+        Me.TextBoxSubject.Size = New System.Drawing.Size(362, 20)
+        Me.TextBoxSubject.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(171, 128)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(31, 20)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "To:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(169, 167)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 20)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Subject:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(167, 227)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(78, 20)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Message:"
+        '
+        'Button3
+        '
+        Me.Button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button3.Location = New System.Drawing.Point(549, 451)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Exit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(167, 372)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(96, 20)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Attachment:"
+        '
+        'TextBoxAttachment
+        '
+        Me.TextBoxAttachment.Location = New System.Drawing.Point(280, 374)
+        Me.TextBoxAttachment.Name = "TextBoxAttachment"
+        Me.TextBoxAttachment.Size = New System.Drawing.Size(278, 20)
+        Me.TextBoxAttachment.TabIndex = 14
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(564, 374)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 15
+        Me.Button4.Text = "......."
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.Makhanlal_University_Bhopal.My.Resources.Resources.mcucut
+        Me.PictureBox3.Location = New System.Drawing.Point(132, 8)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(408, 101)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 16
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.Makhanlal_University_Bhopal.My.Resources.Resources.MCU_SJ_Year
+        Me.PictureBox2.Location = New System.Drawing.Point(7, 4)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(119, 102)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 11
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Makhanlal_University_Bhopal.My.Resources.Resources.MCU_SJ_Year
+        Me.PictureBox1.Location = New System.Drawing.Point(546, 7)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(119, 102)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.Makhanlal_University_Bhopal.My.Resources.Resources._25678_MCU_NEW
+        Me.PictureBox4.Location = New System.Drawing.Point(0, 195)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(161, 302)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 17
+        Me.PictureBox4.TabStop = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.SteelBlue
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.ForeColor = System.Drawing.Color.Maroon
+        Me.Button5.Location = New System.Drawing.Point(10, 148)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(130, 41)
+        Me.Button5.TabIndex = 18
+        Me.Button5.Text = "Go To Mail Section"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(686, 497)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBoxAttachment)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBoxSubject)
+        Me.Controls.Add(Me.TextBoxTo)
+        Me.Controls.Add(Me.RichTextBoxMessage)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Makhanlal University Bhopal"
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents RichTextBoxMessage As System.Windows.Forms.RichTextBox
+    Friend WithEvents TextBoxTo As System.Windows.Forms.TextBox
+    Friend WithEvents TextBoxSubject As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxAttachment As System.Windows.Forms.TextBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+
+End Class
---
+++ Makhanlal_University_Bhopal/Makhanlal_University_Bhopal/Form1.vb
@@ -0,0 +1,106 @@
+﻿Imports System.Speech
+
+Imports System.Net.Mail
+
+
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim MyMailMessage As New MailMessage()
+        Try
+            MyMailMessage.From = New MailAddress("#Enter your mail here")
+            MyMailMessage.To.Add(TextBoxTo.Text)
+            MyMailMessage.Subject = TextBoxSubject.Text
+            MyMailMessage.Body = RichTextBoxMessage.Text
+            If Not OpenFileDialog1.FileName = vbNullString Then
+                Dim attach As New Attachment(OpenFileDialog1.FileName)
+                MyMailMessage.Attachments.Add(attach)
+
+            End If
+
+
+            Dim SMTP As New SmtpClient("smtp.gmail.com")
+            SMTP.Port = 587
+            SMTP.EnableSsl = True
+            SMTP.Credentials = New System.Net.NetworkCredential("#Your Mail", "#Password")
+            SMTP.Send(MyMailMessage)
+            MsgBox("Your Mail has been Successfully sent")
+            TextBoxTo.Clear()
+            TextBoxSubject.Clear()
+            RichTextBoxMessage.Clear()
+            TextBoxAttachment.Clear()
+
+            TextBoxTo.Focus()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        MaximizeBox = True
+
+        TextBoxTo.Hide()
+        TextBoxSubject.Hide()
+        RichTextBoxMessage.Hide()
+        Button1.Hide()
+        Button2.Hide()
+        Label1.Hide()
+        Label2.Hide()
+        Label3.Hide()
+        PictureBox1.Hide()
+        PictureBox2.Hide()
+        PictureBox3.Hide()
+        Button3.Hide()
+        Button4.Hide()
+        PictureBox4.Hide()
+        TextBoxAttachment.Hide()
+        Label4.Hide()
+
+
+
+        WebBrowser1.Navigate(New Uri("http://mcu.ac.in"))
+
+
+    End Sub
+
+    
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Me.Close()
+
+
+
+    End Sub
+
+ 
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        OpenFileDialog1.ShowDialog()
+
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+        TextBoxAttachment.Text$ = OpenFileDialog1.FileName
+
+    End Sub
+
+
+    Private Sub Button5_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+
+
+
+
+
+
+        Me.Controls.Clear()
+
+        InitializeComponent()
+
+
+
+    End Sub
+
+    Private Sub WebBrowser1_DocumentCompleted(ByVal sender As System.Object, ByVal e As System.Windows.Forms.WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+
+    End Sub
+End Class
